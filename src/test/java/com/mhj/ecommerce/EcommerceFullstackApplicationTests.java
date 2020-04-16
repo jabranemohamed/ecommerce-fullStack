f3373d64df036bbd84c8d891938d5b8d7d59780c
mhjabrane <mohamed.jabrane@adservio.fr>
2020-04-16 10:34:57 +0200

    Initial commit

---
+++ src/test/java/com/mhj/ecommerce/EcommerceFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhj.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceFullstackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
